bced2c6e3567f883446a6cc993ea0053328cbd41
NikolasPapas <nikolaspapazian@gmail.com>
2016-01-15 02:34:04 +0200

    unit test 2

--- WindowsFormsApplication2 - Αντιγραφή/UnitTestProject1/UnitTest1.cs
+++ WindowsFormsApplication2 - Αντιγραφή/UnitTestProject1/UnitTest1.cs
@@ -82,7 +82,7 @@ namespace WindowsFormsApplication2.Tests
                 }
             }
             dataretes.deleteitems(code);
-            
+
             Assert.ReferenceEquals(0, corect);
         }
 
@@ -93,24 +93,72 @@ namespace WindowsFormsApplication2.Tests
             dataretes.store();
             int corect = 1;
             string code = "100";
-            int vali =  30;
+            int vali = 30;
             string name = "nikolas";
             string price = "1";
             string foto = "C:/Users/strike xXx/Desktop/mixaniki/018510-glossy-black-3d-button-icon-symbols-shapes-power-button.png";
             string thesh = "A2";
             string avalable = "1";
-            dataretes.addnewobj(name, price, foto, thesh, avalable,code);
+            dataretes.addnewobj(name, price, foto, thesh, avalable, code);
             dataretes.addobj(int.Parse(code), vali);
             dataretes.store();
             for (int i = 0; i < dataretes.len; i++)
             {
-                if (dataretes.table[i, 5] == code & int.Parse(dataretes.table[i, 6]) == vali+1)
+                if (dataretes.table[i, 5] == code & int.Parse(dataretes.table[i, 6]) == vali + 1)
                 {
                     corect = 0;
                     break;
                 }
             }
             dataretes.deleteitems(code);
+            Assert.ReferenceEquals(0, corect);
+        }
+
+        [TestMethod()]
+        public void objpriTest()
+        {
+            Classlistvi dataretes = new Classlistvi();
+            dataretes.store();
+            int corect = 1;
+            string code = "100";
+            int vali = 30;
+            string name = "nikolas";
+            string price = "1";
+            string foto = "C:/Users/strike xXx/Desktop/mixaniki/018510-glossy-black-3d-button-icon-symbols-shapes-power-button.png";
+            string thesh = "A2";
+            string avalable = "1";
+            dataretes.addnewobj(name, price, foto, thesh, avalable, code);
+            dataretes.objpri(int.Parse(code), vali);
+            dataretes.store();
+            for (int i = 0; i < dataretes.len; i++)
+            {
+                if (dataretes.table[i, 5] == code & int.Parse(dataretes.table[i, 2]) == vali)
+                {
+                    corect = 0;
+                    break;
+                }
+            }
+            dataretes.deleteitems(code);
+            Assert.ReferenceEquals(0, corect);
+        }
+
+        [TestMethod()]
+        public void storinsertTest1()
+        {
+            Classlistvi dataretes = new Classlistvi();
+            int corect = 1;
+            dataretes.store();
+            string name = dataretes.stortable[2, 0];
+            string vali = dataretes.stortable[2, 1];
+
+            dataretes.storinsert(1993, name);
+            dataretes.store();
+            if (dataretes.stortable[2, 0] == name & vali == dataretes.stortable[2, 1])
+            {
+                corect = 0;
+            }
+            dataretes.storinsert(int.Parse(vali) , name);
+            dataretes.store();
             Assert.ReferenceEquals(0, corect);
         }
     }
